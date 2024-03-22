c20894eabd3d2100767c68c7898a674e386a456f
EgorKonkin <konkinegor@gmail.com>
2024-03-22 18:32:04 +0300

    init commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div style="background-color: red; padding: 20px 0px; width: 100%"></div>
+    <div
+        style="
+            display: flex;
+            justify-content: center;
+            background-color: #45a1ff;
+            color: #fff;
+            position: absolute;
+            top: calc(100% - 25px);
+            width: 100%;
+        "
+    >
+        sdsafag
+    </div>
+</template>
